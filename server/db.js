9bb6ec2907af8fca70eec56a2754f42890b56e45
Ryan P. Montoya <87250276+ryeact-dev@users.noreply.github.com>
2023-06-24 20:24:00 +0800

    Add files via upload

--- server/db.js
+++ server/db.js
@@ -3,12 +3,21 @@ const path = require("path");
 require("dotenv").config({ path: __dirname + "/.env" });
 // console.log(process.env.USER);
 
+// const pool = new Pool({
+//   user: process.env.USER,
+//   password: process.env.PASSWORD,
+//   host: process.env.HOST,
+//   port: process.env.DBPORT,
+//   database: "todoapp",
+// });
+
 const pool = new Pool({
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  host: process.env.HOST,
-  port: process.env.DBPORT,
-  database: "todoapp",
+  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+});
+
+pool.connect((err) => {
+  if (err) throw new Error();
+  console.log("Connect to PostgreSQL successfully");
 });
 
 module.exports = pool;
