bb622002fd0e9525387c0e0b4444c9da6b26d4c0
Ryan P. Montoya <87250276+ryeact-dev@users.noreply.github.com>
2023-06-24 20:24:23 +0800

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,15 +1,6 @@
 const Pool = require("pg").Pool;
 const path = require("path");
 require("dotenv").config({ path: __dirname + "/.env" });
-// console.log(process.env.USER);
-
-// const pool = new Pool({
-//   user: process.env.USER,
-//   password: process.env.PASSWORD,
-//   host: process.env.HOST,
-//   port: process.env.DBPORT,
-//   database: "todoapp",
-// });
 
 const pool = new Pool({
   connectionString: process.env.POSTGRES_URL + "?sslmode=require",
