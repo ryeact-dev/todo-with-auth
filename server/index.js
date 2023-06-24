5484bad0f1f9421e10aacfc6147cda49b101e52d
Ryan P. Montoya <87250276+ryeact-dev@users.noreply.github.com>
2023-06-24 22:21:15 +0800

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -68,21 +68,21 @@ app.delete("/todos/:id", async (req, res) => {
 
 // signup
 app.post("/signup", async (req, res) => {
-  const { emailValue, passwordValue } = req.body;
+  const { email, password } = req.body;
   const salt = bcrypt.genSaltSync(10);
-  const hashedPassword = bcrypt.hashSync(passwordValue, salt);
+  const hashedPassword = bcrypt.hashSync(password, salt);
 
   try {
     await pool.query(
       `INSERT INTO users (email, hashed_password) VALUES($1, $2)`,
-      [emailValue, hashedPassword]
+      [email, hashedPassword]
     );
 
-    const token = jwt.sign({ emailValue }, "supersecretstring", {
+    const token = jwt.sign({ email }, "supersecretstring", {
       expiresIn: "1hr",
     });
 
-    res.json({ emailValue, token });
+    res.json({ email, token });
   } catch (err) {
     console.error(err);
     if (err) {
@@ -93,19 +93,19 @@ app.post("/signup", async (req, res) => {
 
 // login
 app.post("/login", async (req, res) => {
-  const { emailValue, passwordValue } = req.body;
+  const { email, password } = req.body;
   try {
     const users = await pool.query("SELECT * FROM users WHERE email = $1", [
-      emailValue,
+      email,
     ]);
 
     if (!users.rows.length) return res.json({ error: "User does not exist!" });
 
     const success = await bcrypt.compare(
-      passwordValue,
+      password,
       users.rows[0].hashed_password
     );
-    const token = jwt.sign({ emailValue }, "supersecretstring", {
+    const token = jwt.sign({ email }, "supersecretstring", {
       expiresIn: "1hr",
     });
 
