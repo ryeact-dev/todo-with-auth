a3e186620aca671a5414703a0564e8a48cbd3c9e
Ryan P. Montoya <87250276+ryeact-dev@users.noreply.github.com>
2023-06-24 22:20:59 +0800

    Add files via upload

---
+++ client/src/components/Auth.js
@@ -0,0 +1,78 @@
+import { useState } from "react";
+import { useCookies } from "react-cookie";
+import RegisterForm from "./RegisterForm";
+import LoginForm from "./LoginForm";
+
+const Auth = () => {
+  const setCookie = useCookies(null)[1];
+  const [isLogIn, setIsLogin] = useState(true);
+
+  const viewLogin = (status) => {
+    setIsLogin(status);
+  };
+
+  const submitData = async (email, password) => {
+    try {
+      const userData = {
+        email,
+        password,
+      };
+
+      const endpoint = isLogIn ? "login" : "signup";
+
+      const response = await fetch(
+        `${process.env.REACT_APP_SERVERURL}/${endpoint}`,
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(userData),
+        }
+      );
+
+      const data = await response.json();
+
+      if (data.error) {
+        throw new Error(data.error);
+      }
+      setCookie("Email", data.email);
+      setCookie("AuthToken", data.token);
+      window.location.reload();
+    } catch (err) {
+      alert(err.message);
+    }
+  };
+
+  const btnSwitch = !isLogIn ? "bg-white" : "bg-gray-400";
+  const btnSwitch1 = isLogIn ? "bg-white" : "bg-gray-400";
+
+  return (
+    <div className='w-[500px] rounded-lg overflow-hidden shadow-md bg-gray-50'>
+      <div className={isLogIn ? "h-[320px] px-4 py-2" : "px-4 py-2"}>
+        <h1 className='my-2 text-center font-semibold text-2xl'>
+          {isLogIn ? "Log in" : "Register"}
+        </h1>
+        {isLogIn ? (
+          <LoginForm isLogIn={isLogIn} onFormSubmit={submitData} />
+        ) : (
+          <RegisterForm isLogIn={isLogIn} onFormSubmit={submitData} />
+        )}
+      </div>
+      <div className='flex text-gray-700'>
+        <button
+          onClick={() => viewLogin(false)}
+          className={`w-1/2 border-none p-2 ${btnSwitch}`}
+        >
+          Register
+        </button>
+        <button
+          onClick={() => viewLogin(true)}
+          className={`w-1/2 border-none p-2 ${btnSwitch1}`}
+        >
+          Login
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Auth;
